c39094c5ca582df06d88cecca09cb401752c3064
Philip K <filipposkat@icloud.com>
2022-06-03 19:06:06 +0300

    Added some feature to revenge algorithm

--- omada51_moran.py
+++ omada51_moran.py
@@ -25,21 +25,39 @@ class PlayerOmada51(Player):
         neighbors = self.game_info.g.neighbors(from_node)
         list_of_neighbors = list(neighbors)
         node_types = nx.get_node_attributes(self.game_info.g, "types")
-
+        eg_centralities = nx.eigenvector_centrality(self.game_info.g)
         # Choose first foreign neighbor
         targets = [v for v in list_of_neighbors if node_types[v] != self.my_type]
         node = None
         if targets:
             node = max(targets, key=lambda item: self.game_info.g.degree[item])
-            history=self.game_info.history
-            took_from_me=[move.player_type for move in history if move.type_from==self.my_type]
-            counter=Counter(took_from_me)
-            sorted_counters=sorted(counter.items(), key=lambda kv: kv[1], reverse=True)
-            for t,c in sorted_counters:
-                if t !=self.my_type:
-                    specific_targets=[v for v in list_of_neighbors if node_types[v] == t]
+            history = self.game_info.history
+            took_from_me = [move.player_type for move in history if move.type_from == self.my_type]
+            counter = Counter(took_from_me)
+            sorted_counters = sorted(counter.items(), key=lambda kv: kv[1], reverse=True)  # sorted list of players based on attacks on us
+            attacks_dict = {}  # dictionary containing attacks from each player
+            for item in counter.items():
+                if item[0] not in attacks_dict:
+                    attacks_dict[item[0]] += item[1]
+            cntr, num_of_distinct_type_values, distinct_type_keys, distinct_type_counts = self.game_info.get_number_of_active_players() # gets counte
+            n_of_nodes_by_player = {}
+            for item in cntr.items():
+                if item[0] not in n_of_nodes_by_player:
+                    n_of_nodes_by_player[item[0]] = item[1]
+
+            for t, c in sorted_counters:
+                if t != self.my_type:
+                    count = attacks_dict[t]
+                    same_count = []
+                    # Check if other players have the same count
+                    for p in attacks_dict:
+                        if count == attacks_dict[p] and p != self.my_type:
+                            same_count.append(p)
+                    same_count.sort(key=lambda plr:n_of_nodes_by_player[plr])
+
+                    specific_targets = [v for v in list_of_neighbors if node_types[v] == t]
                     if specific_targets:
                         node=max(specific_targets, key=lambda item:self.game_info.g.degree[item])
+                        #node = max(specific_targets, key=lambda item: eg_centralities[item])
                         return node
         return node
-
